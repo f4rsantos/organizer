018edd746fc24ad1a20cc163922bab8e974938c5
Fernando Santos <f4rsantos@gmail.com>
2026-07-07 01:00:27 +0100

    Delete src/langs.js

--- src/langs.js
+++
@@ -1,9 +0,0 @@
-export const LANGS = [
-  { code: 'en', label: 'English', short: 'EN' },
-  { code: 'pt', label: 'Português', short: 'PT' },
-  { code: 'fr', label: 'Français', short: 'FR' },
-  { code: 'de', label: 'Deutsch', short: 'DE' },
-  { code: 'es', label: 'Español', short: 'ES' },
-]
-
-export const LANG_CODES = LANGS.map(l => l.code)
