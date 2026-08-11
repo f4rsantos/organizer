51e14baf90499fd214865aa3046dc8d0cbc9bf20
Fernando Santos <frsantos@ua.pt>
2026-08-12 00:11:16 +0100

    fix: icon

--- vite.config.js
+++ vite.config.js
@@ -45,7 +45,9 @@ export default defineConfig({
         icons: [
           { src: 'favicon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any' },
           { src: 'pwa-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: 'pwa-256.png', sizes: '256x256', type: 'image/png', purpose: 'any' },
           { src: 'pwa-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+          { src: 'pwa-1024.png', sizes: '1024x1024', type: 'image/png', purpose: 'any' },
           { src: 'pwa-maskable-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ],
         shortcuts: [
